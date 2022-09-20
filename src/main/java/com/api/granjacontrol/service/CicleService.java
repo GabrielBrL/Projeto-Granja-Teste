0fcb836f1d8df142d3649b9544a8e121b23def7d
Gabriel Barros <gabriellourencobarros02@gmail.com>
2022-09-20 18:56:06 -0300

    Adicionando novo projeto

---
+++ src/main/java/com/api/granjacontrol/service/CicleService.java
@@ -0,0 +1,44 @@
+package com.api.granjacontrol.service;
+
+import com.api.granjacontrol.model.CadastroCiclo;
+import com.api.granjacontrol.repository.CadastroCicloRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class CicleService {
+
+    final CadastroCicloRepository cadastroCicloRepository;
+
+    public CicleService(CadastroCicloRepository cadastroCicloRepository) {
+        this.cadastroCicloRepository = cadastroCicloRepository;
+    }
+
+    @Transactional
+    public CadastroCiclo save(CadastroCiclo cadastroCiclo) {
+        return cadastroCicloRepository.save(cadastroCiclo);
+    }
+
+    public Page<CadastroCiclo> findAll(Pageable pageable) {
+        return cadastroCicloRepository.findAll(pageable);
+    }
+
+    public List<CadastroCiclo> findAll() {
+        return cadastroCicloRepository.findAll();
+    }
+
+    public Optional<CadastroCiclo> findById(UUID id) {
+        return cadastroCicloRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(CadastroCiclo cadastroCiclo) {
+        cadastroCicloRepository.delete(cadastroCiclo);
+    }
+}
